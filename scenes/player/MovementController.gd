437dd1bf14873a9bff09d51dfd74ce7bec535e16
unknown <14879968+AlecVD@users.noreply.github.com>
2023-02-22 19:36:45 -0800

    more

--- scenes/player/MovementController.gd
+++ scenes/player/MovementController.gd
@@ -1,31 +1,61 @@
 extends CharacterBody3D
+class_name MovementController
 
 
-const SPEED = 5.0
-const JUMP_VELOCITY = 4.5
+@export var gravity_multiplier := 3.0
+@export var speed := 10
+@export var acceleration := 8
+@export var deceleration := 10
+@export_range(0.0, 1.0, 0.05) var air_control := 0.3
+@export var jump_height := 10
+var direction := Vector3()
+var input_axis := Vector2()
+# Get the gravity from the project settings to be synced with RigidDynamicBody nodes.
+@onready var gravity: float = (ProjectSettings.get_setting("physics/3d/default_gravity") 
+		* gravity_multiplier)
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
 
-
-func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity.y -= gravity * delta
-
-	# Handle Jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
-		velocity.y = JUMP_VELOCITY
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+# Called every physics tick. 'delta' is constant
+func _physics_process(delta: float) -> void:
+	input_axis = Input.get_vector(&"ui_down", &"ui_up",
+			&"ui_left", &"ui_right")
+	
+	direction_input()
+	
+	if is_on_floor():
+		if Input.is_action_just_pressed(&"ui_accept"):
+			velocity.y = jump_height
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		velocity.y -= gravity * delta
+	
+	accelerate(delta)
+	
 	move_and_slide()
+
+
+func direction_input() -> void:
+	direction = Vector3()
+	var aim: Basis = get_global_transform().basis
+	direction = aim.z * -input_axis.x + aim.x * input_axis.y
+
+
+func accelerate(delta: float) -> void:
+	# Using only the horizontal velocity, interpolate towards the input.
+	var temp_vel := velocity
+	temp_vel.y = 0
+	
+	var temp_accel: float
+	var target: Vector3 = direction * speed
+	
+	if direction.dot(temp_vel) > 0:
+		temp_accel = acceleration
+	else:
+		temp_accel = deceleration
+	
+	if not is_on_floor():
+		temp_accel *= air_control
+	
+	temp_vel = temp_vel.lerp(target, temp_accel * delta)
+	
+	velocity.x = temp_vel.x
+	velocity.z = temp_vel.z
